2cccfa6f2a1b04b37d828cd543dc7deb7a7db48b
ragnar764 <58563965+ragnar764@users.noreply.github.com>
2022-03-30 13:14:08 +0300

    Upload Form functionality added

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
-
+    // eslint-disable-next-line
+const [file,setFile] = useState(null);
+const types = ['image/png','image/jpeg', 'image/jpg'];
+const[error,setError] = useState(null);
 const changeHandler = (e) => {
-    console.log('uploaded');
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if(selected && types.includes(selected.type)) {
+        setFile(selected);
+        setError('');
+    } else {
+        setFile(null);
+        setError('Please select an image file (png, jpeg or jpg)');
+    }
+        
 }
 
     return(
         <form>
         <input type="file" onChange={changeHandler} />
+        <div className="output">
+            {error && <div className="error">{ error }</div>}
+            {file && <div>{file.name}</div>}
+        </div>
         </form>
 
     )
---
+++ src/hooks/useStorage.js
@@ -0,0 +1,3 @@
+import { useStatem ,useEfect } from "react";
+import { projectStorage } from "../firebase/config";
+
